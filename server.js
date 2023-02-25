32499237d802ac1a07f05f77fd7c39ef07d888e6
MyroslavaMM <104752856+MyroslavaMM@users.noreply.github.com>
2023-02-25 15:02:53 +0200

    fixed npm start command

--- server.js
+++ server.js
@@ -8,12 +8,14 @@ app.use(
     origin: "http://127.0.0.1:3000"
   })
 );
+app.use(express.static("public"));
 
 app.get("/currency", async function (req, res) {
   const result = await axios.get("http://api.privatbank.ua/p24api/pubinfo?json&exchange&coursid=5");
+  res.header("Access-Control-Allow-Origin", "http://localhost:3000");
   res.send(result.data);
 });
 
 app.listen(8080, () => {
-  console.log("connected");
+  console.log("alive");
 });
