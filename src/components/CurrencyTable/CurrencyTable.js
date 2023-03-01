f11da506de08dafbb9a0b36c418ce0b1b1fc7aaf
MyroslavaMM <104752856+MyroslavaMM@users.noreply.github.com>
2023-03-01 13:00:01 +0200

    fixed data render. localstorage count

--- src/components/CurrencyTable/CurrencyTable.js
+++ src/components/CurrencyTable/CurrencyTable.js
@@ -1,67 +1,95 @@
 import React from "react";
 import { useEffect, useState } from "react";
 import { useSelector, useDispatch } from "react-redux";
-import { getExchangeValues, selectExchangeValues } from "../../reducers/exchangeReducer/index";
+import { getExchangeValues, selectExchangeValues, updateValue, selectError, resetError } from "../../reducers/exchangeReducer/index";
+import { OPERATION } from "../CurrencyExchange/CurrencyExchange";
 import "./CurrencyTable.css";
 
 function CurrencyTable() {
   const dispatch = useDispatch();
   const currency = useSelector(selectExchangeValues);
+  const storeError = useSelector(selectError);
 
-  const [newValue, setNewValue] = useState("");
-  let [count, setCount] = useState(0);
-  const increment = () => setCount(count++);
+  const [activeValue, setActiveNewValue] = useState("");
+  const [activeCCY, setActiveCCY] = useState("");
+  const [activeType, setActiveType] = useState("");
+  const [activeInput, setActiveInput] = useState(false);
 
   useEffect(() => {
     dispatch(getExchangeValues());
-    increment();
-    window.localStorage.setItem("count", count);
   }, []);
 
-  const handleClickEdit = (e) => {
-    const child = e.currentTarget;
-    const parent = child.parentNode;
-    let input = parent.childNodes[0];
-
-    child.style.display = "none";
-
-    const span = document.createElement("span");
-    span.innerHTML = "✕";
-    span.classList.add("cross");
-    span.classList.add("icon");
-
-    parent.appendChild(span);
-
-    span.addEventListener("click", () => {
-      span.style.display = "none";
-      child.style.display = "inline-block";
-      input.disabled = true;
-    });
-
-    input.disabled = false;
+  const handleClickEdit = (rate, ccy, operation) => {
+    setActiveInput(true);
+    setActiveCCY(ccy);
+    setActiveType(operation);
+    setActiveNewValue(rate);
   };
 
-  const changeValue = (e) => {
-    const currencyValue = e.target.value;
-    setNewValue(currencyValue);
+  const handleSave = () => {
+    setActiveInput(false);
+    dispatch(updateValue({ ccy: activeCCY, operation: activeType, rate: activeValue }));
   };
 
-  const resetStorageValues = () => {
-    localStorage.clear();
+  const handleClose = () => {
+    setActiveInput(false);
+    setActiveCCY("");
+    setActiveType("");
+    setActiveNewValue("");
   };
 
-  const table = document.getElementsByClassName("table");
+  const changeValue = (event) => {
+    const currencyValue = event.target.value;
+    setActiveNewValue(currencyValue);
+  };
 
-  if (localStorage.getItem("count") === "5") {
-    if (table.length > 0) {
-      table[0].style.display = "none";
-    }
+  const resetStorageError = () => {
+    dispatch(resetError());
+    dispatch(getExchangeValues());
+  };
+
+  const renderButtons = () => {
     return (
-      <div className="error">
-        <p className="error-message">The localStorage is full</p>
-        <button className="reset" onClick={resetStorageValues}>
-          Reset localStorage
-        </button>
+      <>
+        <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 512 512" className="check-mark" onClick={handleSave}>
+          <path d="M470.6 105.4c12.5 12.5 12.5 32.8 0 45.3l-256 256c-12.5 12.5-32.8 12.5-45.3 0l-128-128c-12.5-12.5-12.5-32.8 0-45.3s32.8-12.5 45.3 0L192 338.7 425.4 105.4c12.5-12.5 32.8-12.5 45.3 0z" />
+        </svg>
+        <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 320 512" className="cross-icon" onClick={handleClose}>
+          <path d="M310.6 150.6c12.5-12.5 12.5-32.8 0-45.3s-32.8-12.5-45.3 0L160 210.7 54.6 105.4c-12.5-12.5-32.8-12.5-45.3 0s-12.5 32.8 0 45.3L114.7 256 9.4 361.4c-12.5 12.5-12.5 32.8 0 45.3s32.8 12.5 45.3 0L160 301.3 265.4 406.6c12.5 12.5 32.8 12.5 45.3 0s12.5-32.8 0-45.3L205.3 256 310.6 150.6z" />
+        </svg>
+      </>
+    );
+  };
+
+  const renderOptions = (rate, operation, ccy) => {
+    if (activeInput === true && activeCCY === ccy && activeType === operation) {
+      return (
+        <>
+          <input className="input" value={activeValue} onChange={changeValue} />
+          {renderButtons()}
+        </>
+      );
+    } else {
+      return (
+        <>
+          <input className="input" value={rate} disabled />
+          <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 512 512" className="edit-icon" onClick={() => handleClickEdit(rate, ccy, operation)}>
+            <path d="M362.7 19.3L314.3 67.7 444.3 197.7l48.4-48.4c25-25 25-65.5 0-90.5L453.3 19.3c-25-25-65.5-25-90.5 0zm-71 71L58.6 323.5c-10.4 10.4-18 23.3-22.2 37.4L1 481.2C-1.5 489.7 .8 498.8 7 505s15.3 8.5 23.7 6.1l120.3-35.4c14.1-4.2 27-11.8 37.4-22.2L421.7 220.3 291.7 90.3z" />
+          </svg>
+        </>
+      );
+    }
+  };
+
+  if (storeError === true) {
+    return (
+      <div>
+        <div className="error">
+          <p className="error-message">The localStorage is full</p>
+          <button className="reset btn btn-warning" onClick={resetStorageError}>
+            Reset localStorage
+          </button>
+        </div>
       </div>
     );
   }
@@ -79,34 +107,26 @@ function CurrencyTable() {
           <td className="headlines-item item">
             {ccy}/{base_ccy}
           </td>
-          <td className="buy item">
-            <input className="input_buy" defaultValue={buyNumber} onChange={changeValue} disabled />
-            <span className="edit icon" onClick={handleClickEdit}>
-              ✐
-            </span>
-          </td>
-          <td className="sale item">
-            <input className="input_sale" defaultValue={saleNumber} onChange={changeValue} disabled />
-            <span className="edit icon" onClick={handleClickEdit}>
-              ✐
-            </span>
-          </td>
+          <td className="buy item">{renderOptions(buyNumber, OPERATION.BUY, ccy)}</td>
+          <td className="sale item">{renderOptions(saleNumber, OPERATION.SALE, ccy)}</td>
         </tr>
       );
     });
   };
 
   return (
-    <table className="table-item ">
-      <thead className="headlines">
-        <tr className="row-item">
-          <th className="headlines-item item">Currency/Current Date</th>
-          <th className="headlines-item item">Buy</th>
-          <th className="headlines-item item">Sale</th>
-        </tr>
-      </thead>
-      <tbody>{renderValues()}</tbody>
-    </table>
+    <>
+      <table className="table-item ">
+        <thead className="headlines">
+          <tr className="row-item">
+            <th className="headlines-item item">Currency/Current Date</th>
+            <th className="headlines-item item">Buy</th>
+            <th className="headlines-item item">Sale</th>
+          </tr>
+        </thead>
+        <tbody>{renderValues()}</tbody>
+      </table>
+    </>
   );
 }
 
--- src/reducers/exchangeReducer/index.js
+++ src/reducers/exchangeReducer/index.js
@@ -2,39 +2,55 @@ import axios from "axios";
 import { createAsyncThunk, createSlice } from "@reduxjs/toolkit";
 
 export const getExchangeValues = createAsyncThunk("currency/getExchangeValues", async () => {
-  try {
-    const response = await axios.get("http://localhost:8080/currency");
-    return response.data;
-  } catch (e) {
-    console.error(e);
+  const requestCount = parseInt(window.localStorage.getItem("requestCount")) || 0;
+
+  if (requestCount > 5) {
+    throw new Error();
   }
+  window.localStorage.setItem("requestCount", requestCount + 1);
+  const response = await axios.get("http://localhost:8080/currency");
+  return response.data;
 });
 
 const exchangeValuesSlice = createSlice({
   name: "exchangeValues",
   initialState: {
-    exchangeValues: []
+    exchangeValues: [],
+    isError: false
   },
   reducers: {
-    valuesBuyUpdated(state, action) {
-      const { ccy, base_ccy, buy } = action.payload;
-      const existingValue = state.find((item) => item.ccy === ccy && item.base_ccy === base_ccy);
-      if (existingValue) {
-        existingValue = buy;
-      }
+    updateValue(state, action) {
+      const { ccy, operation, rate } = action.payload;
+      state.exchangeValues = state.exchangeValues.map((data) => {
+        if (data.ccy === ccy) {
+          data[operation] = rate;
+        }
+        return data;
+      });
+    },
+    resetError(state, action) {
+      state.isError = false;
+      window.localStorage.setItem("requestCount", 0);
     }
   },
   extraReducers: (builder) => {
     builder.addCase(getExchangeValues.fulfilled, (state, action) => {
       state.exchangeValues = action.payload;
     });
+    builder.addCase(getExchangeValues.rejected, (state, action) => {
+      state.isError = true;
+    });
   }
 });
 
-export const { valuesBuyUpdated } = exchangeValuesSlice.actions;
+export const { updateValue, resetError } = exchangeValuesSlice.actions;
 
 export default exchangeValuesSlice.reducer;
 
 export const selectExchangeValues = (state) => {
   return state.exchangeValuesReducer.exchangeValues;
+};
+
+export const selectError = (state) => {
+  return state.exchangeValuesReducer.isError;
 };
